749a7845de2a51e105d3d7d37d6bd3badce076a8
Schmuvius <67434780+Schmuvius@users.noreply.github.com>
2025-08-27 15:19:03 +0200

    Update PlayerService.java to use player targeting instead of mob targeting

---
+++ src/main/java/dev/gallon/motorassistance/common/services/PlayerService.java
@@ -0,0 +1,68 @@
+package dev.gallon.motorassistance.common.services;
+
+import dev.gallon.motorassistance.common.domain.Position;
+import dev.gallon.motorassistance.common.domain.Rotation;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.ClipContext;
+import net.minecraft.util.Mth;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class PlayerService extends EntityService {
+    public PlayerService(@NotNull Player entity) {
+        super(entity);
+    }
+
+    public void setRotation(Rotation rotation) {
+        this.entity.setXRot(rotation.pitch());
+        this.entity.setYRot(rotation.yaw());
+    }
+
+    public boolean canInteract() {
+        return this.entity.isPickable();
+    }
+
+    // Renamed from findMobsAroundPlayer to findPlayersAroundPlayer
+    public List<EntityService> findPlayersAroundPlayer(double range) {
+        return entity
+                .level()
+                .getEntitiesOfClass(
+                        Player.class,
+                        new AABB(
+                                entity.getX() - range,
+                                entity.getY() - range,
+                                entity.getZ() - range,
+                                entity.getX() + range,
+                                entity.getY() + range,
+                                entity.getZ() + range
+                        ),
+                        player -> player != entity // Exclude the current player
+                )
+                .stream()
+                .map(EntityService::new)
+                .toList();
+    }
+
+    public BlockService rayTrace(double reach, Position source, Rotation direction) {
+        float f2 = Mth.cos((float) (-direction.yaw() * (Math.PI / 180.0) - Math.PI));
+        float f3 = Mth.sin((float) (-direction.yaw() * (Math.PI / 180.0) - Math.PI));
+        float f4 = -Mth.cos((float) (-direction.pitch() * (Math.PI / 180.0)));
+        float f5 = Mth.sin((float) (-direction.pitch() * (Math.PI / 180.0)));
+        float f6 = f3 * f4;
+        float f7 = f2 * f4;
+        Vec3 vector = source.toVec3().add(f6 * reach, f5 * reach, f7 * reach);
+
+        return new BlockService(entity.level().clip(
+                new ClipContext(
+                        source.toVec3(),
+                        vector,
+                        ClipContext.Block.OUTLINE,
+                        ClipContext.Fluid.NONE,
+                        entity
+                )
+        ));
+    }
+}
